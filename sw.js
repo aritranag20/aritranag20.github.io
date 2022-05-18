12004a73c02cde9b525b3ccaeea1b57501eca6ca
Aritra Nag <Aritra.Nag@husqvarnagroup.com>
2022-05-18 09:04:19 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,35 +27,27 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-71fca44fd770e73284fd.js"
+    "url": "webpack-runtime-fe37250305421c011912.js"
   },
   {
     "url": "framework-fbf2011d6c2b83ba228a.js"
   },
   {
-    "url": "app-a5051990be87a4fd760c.js"
+    "url": "app-c82b853f2f21ed65a64e.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0c08a97c93cc7eb0083d3ba2c740318e"
+    "revision": "4a657db66499a5cc81fd2e816e995a99"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-27c66d5dcd0cba7d37a1.js"
-  },
-  {
-    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "f2c002077289a7e1ac538802bc7f5314"
-  },
-  {
-    "url": "page-data/app-data.json",
-    "revision": "0d5caa8d8169f339944f86f9ec214ab2"
   },
   {
     "url": "polyfill-b900ffefca49c091a1d3.js"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "5a77d47ddb196306539363c8017ababd"
+    "revision": "f21ac93dcd2dcfbf1207ddaa911d229f"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -142,12 +134,12 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   lastNavigationRequest = event.request.url
 
   let { pathname } = new URL(event.request.url)
-  pathname = pathname.replace(new RegExp(`^/aritranag20.github.io`), ``)
+  pathname = pathname.replace(new RegExp(`^`), ``)
 
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/aritranag20.github.io/app-a5051990be87a4fd760c.js`))) {
+  if (!resources || !(await caches.match(`/app-c82b853f2f21ed65a64e.js`))) {
     return await fetch(event.request)
   }
 
@@ -160,7 +152,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
     }
   }
 
-  const offlineShell = `/aritranag20.github.io/offline-plugin-app-shell-fallback/index.html`
+  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
   const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
   return await caches.match(offlineShellWithKey)
 })
